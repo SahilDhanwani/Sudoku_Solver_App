8dfe93f785dfc0ba6b3f6c77e9c481fbdf999a29
SahilDhanwani <sahildhanwani291203@gmail.com>
2025-06-19 21:46:36 +0530

    Rearranged frame initialization in MainClass to prioritize Frame_1 over Frame_3

--- src/sudoku_Game_Solver/MainClass.java
+++ src/sudoku_Game_Solver/MainClass.java
@@ -1,10 +1,10 @@
 package sudoku_Game_Solver;
 
-public class MainClass {
+public class MainClass {                
     public static void main(String[] args) {
         System.out.println("Hello world");
-        // new Frame_1();
-        new Frame_3();
-        return;
+        new Frame_1();
+        // new Frame_3();
+        return; 
     }
 }
